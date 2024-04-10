d5d324aefe7b304cf69520855afa29f1d768a00d
Kroger-1 <148895862+Kroger-1@users.noreply.github.com>
2024-04-10 13:42:40 -0400

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Task {
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+class Task implements Comparable<Task>, Iterable{
+    private String taskTitle;
+    private String taskDescription;
+    private int taskPriority;
+
+    public Task(String taskTitle, String taskDescription, int taskPriority) {
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+        this.taskPriority = taskPriority;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public int getTaskPriority() {
+        return taskPriority;
+    }
+
+    public void setTaskPriority(int taskPriority) {
+        this.taskPriority = taskPriority;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskTitle='" + taskTitle + '\'' +
+                ", taskDescription='" + taskDescription + '\'' +
+                ", taskPriority=" + taskPriority +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        if (taskPriority > o.taskPriority) {
+            return -1;
+        } else if (taskPriority < o.taskPriority) {
+            return 1;
+        } else {
+            return this.taskTitle.compareTo(o.taskTitle);
+        }
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return Iterable.super.spliterator();
+    }
 }
